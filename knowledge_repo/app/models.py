a75ec6fa387e515bdbf960d20b329758b8966f39
Niharika Ray <nikki.ray@airbnb.com>
2016-11-03 01:27:03 -0700

    [Permissions] Add the UI view into groups

--- knowledge_repo/app/models.py
+++ knowledge_repo/app/models.py
@@ -350,7 +350,12 @@ class Post(db.Model):
 
         # create an implicit group, group_post.id, to add
         # single users to
-        group = Group(name="group_" + str(self.id))
+        # we don't have self.id here necessarily, so query the Post table
+        # for the path
+        post_id = (db_session.query(Post)
+                             .filter(Post.path == self.path)
+                             .first()).id
+        group = Group(name="group_" + str(post_id))
 
         # this created group should have the author associated with it
         # so they can add people to the post
@@ -502,6 +507,8 @@ class Post(db.Model):
 
         self.status = kp.status
 
+        self.private = 0
+        # we do this check so that no header (None) and False are treated the same
         if headers.get('private', ''):
             self.private = 1
             self.groups = headers.get('allowed_groups', [])
@@ -542,4 +549,4 @@ class Group(db.Model):
 
     @users.setter
     def users(self, user_objs):
-        self._users = user_objs
+        self._users = self._users + user_objs
